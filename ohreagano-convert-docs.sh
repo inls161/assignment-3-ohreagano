5ea33393156019f0f0447dffd3d5d00f6722f353
Reagan Cline <reaganecline@gmail.com>
2016-10-26 14:27:54 +0000

    added original writing FederalistPaper.md

--- ohreagano-convert-docs.sh
+++ ohreagano-convert-docs.sh
@@ -3,16 +3,16 @@
 # Declare input argument as a variable
 INPUT=$1
 
-#
+# 
 echo "Going to convert $INPUT to HTML, DOCX, ODT, and PDF"
 
-# Request name for file
+# Request name for output files
 echo "What do you want to name the converted files?"
 
-# Read in the file name
+# Read in file name
 read FILENAME
 
-# tell user it is working
+# Tell user the script is converting files
 echo "Converting $INPUT to $FILENAME..."
 
 # Convert markdown to HTML
@@ -27,5 +27,5 @@ pandoc -S -o $FILENAME.odt $INPUT
 # Convert markdown to PDF
 pandoc -S -o $FILENAME.pdf $INPUT
 
-# Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF"
+# Tell user script has converted files to 4 output formats
 echo "Converted $1 to HTML, DOCX, ODT, and PDF"
